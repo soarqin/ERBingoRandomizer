b95b760bac141e27184357cc9f3865fd116e4e7e
Soar Qin <soarchin@gmail.com>
2024-04-05 00:57:38 +0800

    Support for multilanguages

--- src/ERBingoRandomizer/Commands/LaunchEldenRingCommand.cs
+++ src/ERBingoRandomizer/Commands/LaunchEldenRingCommand.cs
@@ -14,15 +14,15 @@ public class LaunchEldenRingCommand : CommandBase {
         return _mwViewModel.FilesReady && !_mwViewModel.InProgress;
     }
     public override void Execute(object? parameter) {
-        if (eldenRingIsOpen()) {
-            _mwViewModel.DisplayMessage("Elden Ring is still open. Please close Elden Ring or wait for it to full exit.");
+        if (EldenRingIsOpen()) {
+            _mwViewModel.DisplayMessage("法环还在运行，请彻底关闭游戏后再次点击");
             return;
         }
         _mwViewModel.ListBoxDisplay.Clear();
-        _mwViewModel.DisplayMessage("Launching Elden Ring via ModEngine 2");
-        launchEldenRing();
+        _mwViewModel.DisplayMessage("正在通过ModEngine 2运行艾尔登法环");
+        LaunchGame();
     }
-    private static void launchEldenRing() {
+    private static void LaunchGame() {
         Process me2 = new() {
             StartInfo = new ProcessStartInfo {
                 FileName = "launchmod_bingo.bat",
@@ -33,7 +33,54 @@ public class LaunchEldenRingCommand : CommandBase {
         };
         me2.Start();
     }
-    private static bool eldenRingIsOpen() {
+    private static bool EldenRingIsOpen() {
+        Process[] processes = Process.GetProcesses();
+        foreach (Process process in processes) {
+            if (process.ProcessName is not "eldenring") {
+                continue;
+            }
+            return true;
+        }
+        return false;
+    }
+    private void ViewModel_PropertyChanged(object? sender, PropertyChangedEventArgs e) {
+        if (e.PropertyName is nameof(MainWindowViewModel.FilesReady)
+            or nameof(MainWindowViewModel.InProgress)) {
+            OnCanExecuteChanged();
+        }
+    }
+}
+
+public class LaunchEldenRingNormalCommand : CommandBase {
+    private readonly MainWindowViewModel _mwViewModel;
+    public LaunchEldenRingNormalCommand(MainWindowViewModel mwViewModel) {
+        _mwViewModel = mwViewModel;
+        _mwViewModel.PropertyChanged += ViewModel_PropertyChanged;
+    }
+    public override bool CanExecute(object? parameter) {
+        return _mwViewModel.FilesReady && !_mwViewModel.InProgress;
+    }
+    public override void Execute(object? parameter) {
+        if (EldenRingIsOpen()) {
+            _mwViewModel.DisplayMessage("法环还在运行，请彻底关闭游戏后再次点击");
+            return;
+        }
+        _mwViewModel.ListBoxDisplay.Clear();
+        _mwViewModel.DisplayMessage("正在通过ModEngine 2运行艾尔登法环");
+        LaunchGame();
+    }
+    private static void LaunchGame() {
+        Process me2 = new() {
+            StartInfo = new ProcessStartInfo {
+                FileName = "launchmod_normal.bat",
+                WorkingDirectory = Const.ME2Path,
+                UseShellExecute = true,
+                CreateNoWindow = true,
+            },
+        };
+        me2.Start();
+    }
+    private static bool EldenRingIsOpen() {
         Process[] processes = Process.GetProcesses();
         foreach (Process process in processes) {
             if (process.ProcessName is not "eldenring") {
